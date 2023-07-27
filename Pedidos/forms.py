0fdf661f84980e58bad0dd59f9139840873c7d4c
Ramon <ramon26player@gmail.com>
2023-07-27 17:47:11 -0300

    Mexendo com desenho part2

--- Pedidos/forms.py
+++ Pedidos/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from .models import Perfil
-from .models import Pedido
+from .models import Perfil, Pedido
 
-class PedidoForm(forms.Form):
+class PedidoForm(forms.ModelForm):
     perfil = forms.ChoiceField(
         label='Perfil',
         queryset=Perfil.objects.all(),
